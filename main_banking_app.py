b956e67f8a60e3bbcf710b315a156939e4fe3816
jschmiss <julian.schmiss@holidaycheck.com>
2018-11-05 09:24:59 +0100

    update

--- main_banking_app.py
+++ main_banking_app.py
@@ -9,5 +9,3 @@ DKB_ACC = os.environ.get('DKB_ACC')
 client_dkb = FinTS3PinTanClient('12030000', DKB_ACC, DKB_PW, 'https://banking-dkb.s-fints-pt-dkb.de/fints30')
 
 konten = client_dkb.get_sepa_accounts()
-
-balance = client_dkb.get_holdings(konten[0])
